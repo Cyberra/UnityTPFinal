933ecedabdc1b452f97a9b5a4c44973ca205f02b
Max <1430070@P-15.college.bart.qc.ca>
2015-07-06 12:07:38 -0400

    First commit.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public GameObject player = null;
+
+	private Vector3 cameraOffSet = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		cameraOffSet = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateCamera ();
+	}
+
+	void UpdateCamera(){
+		if (player != null) {
+			transform.LookAt(player.transform);
+		}
+		
+		transform.position = player.transform.position + cameraOffSet;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myBody;
+	private float playerSpeed = 1.0f;
+	private const float playerMaxSpeed = 5.0f;
+	private bool isMoving;
+
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myControls ();
+	}
+
+	void myControls() {
+		Debug.Log (Input.GetAxis("Horizontal"));
+
+		if (Input.GetKey("d")){
+			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey("a")) {
+			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
+		}
+	}
+}
