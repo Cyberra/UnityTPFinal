58a80c04064e603922f00eb32262beff2c2afc81
Max <1430070@P-15.college.bart.qc.ca>
2015-07-21 16:06:22 -0400

    Loc not quite working

---
+++ Assets/Scripts/ApplyLoc.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ApplyLoc : MonoBehaviour 
+{
+    private Localizater loc;
+    private Text text;
+
+	void Start () 
+    {
+        loc = Localizater.FindObjectOfType<Localizater>();
+        text = GetComponent<Text>();
+
+        if (text.tag == "StartButton")
+        {
+            //text.text = loc.IDToWord("Start");
+			loc.IDToWord("Start");
+        }
+
+        if (text.tag == "QuitButton")
+        {
+
+        }
+	}
+}
--- Assets/Scripts/GUIBlock.cs
+++ Assets/Scripts/GUIBlock.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GUI : MonoBehaviour 
+public class GUIBlock : MonoBehaviour 
 {
     // Player reference.
     private Player myPlayer;
