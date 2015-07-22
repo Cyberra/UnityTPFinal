05f3892ea6e68aa67a837eb2c23608fe8eba4798
Max <Max__Beaulieu@hotmail.com>
2015-07-22 00:12:36 -0400

    Localization done with switch case :D

--- Assets/Scripts/ApplyLoc.cs
+++ Assets/Scripts/ApplyLoc.cs
@@ -4,23 +4,48 @@ using System.Collections;
 
 public class ApplyLoc : MonoBehaviour 
 {
+    // Variables used to set text.
     private Localizater loc;
     private Text text;
 
 	void Start () 
     {
+        // Get my localization gameobject on the scene.
         loc = Localizater.FindObjectOfType<Localizater>();
+        // Get the text component.
         text = GetComponent<Text>();
 
-        if (text.tag == "StartButton")
+        // Determine which tag is associated and write the correct text.
+        switch(text.tag)
         {
-            //text.text = loc.IDToWord("Start");
-			loc.IDToWord("Start");
-        }
-
-        if (text.tag == "QuitButton")
-        {
-
+            // Each case calls a function listed below.
+            case "StartButton":
+                StartButton("Start");
+                break;
+            case "QuitButton":
+                QuitButton("Quit");
+                break;
+            case "ReplayButton":
+                ReplayButton("Replay");
+                break;
+            default:
+                break;
         }
 	}
+
+    // All functions generating texts.
+    private void StartButton(string ID)
+    {
+        text.text = loc.IDToWord(ID);
+    }
+
+    private void QuitButton(string ID)
+    {
+        text.text = loc.IDToWord(ID);
+    }
+
+    private void ReplayButton(string ID)
+    {
+        text.text = loc.IDToWord(ID);
+    }
 }
